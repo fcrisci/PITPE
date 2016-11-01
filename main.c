29260fd638e0beadd86a02f313310130850c6112
Panzer12 <lhuaman@itba.edu.ar>
2016-11-01 15:58:15 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "getnum.h"
+#include "valida.h" // biblioteca con funciones valida 
+#include <stdlib.h>
+int main ()
+{
+	int opcion;
+	printf("1.Juego de dos jugadores\n 2.Juego contra computadora \n 3.Recuperar un juego guardado \n 4.Terminar\n ");
+	opcion=menu()
+    
+
+}
+int menu(void)
+{
+	int a;
+	do {
+    a=getint("Ingrese opcion\n");
+    if (validamenu(a))  //opcion<1 || opcion>4 devuelve 1 o 0
+    	printf("Error 1\n");
+
+	}
+	while (!validamenu(a))
+	return a;
+}
