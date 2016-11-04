e15bd3c5fb4dd404df7bc56376c35ca3ddc9f981
Panzer12 <lhuaman@itba.edu.ar>
2016-11-04 19:06:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
-#include "getnum.h"
-#include "valida.h" // biblioteca con funciones valida 
-#include "buttonsAndScissorsBack.h"
+#include "buttonsAndScissorsBack.h" // incluimos adentro el "getint.h"
+#include "valida.h> //opcionalmente separamos una biblioteca aparte del backend que se llama valida
 #include <stdlib.h>
+int validaopcion(int a);
 int main ()
 {
-	int opcion;
-	printf("1.Juego de dos jugadores\n 2.Juego contra computadora \n 3.Recuperar un juego guardado \n 4.Terminar\n ");
-	opcion=menu()
-    
+    int opcion;
+    printf (" 1.Juego de dos jugadores\n 2.Juego contra computadora \n 3.Recuperar un juego \n 4.Terminar \n");
+    do {
+        opcion=getint("Elija opción \n");
+        if (validaopcion(opcion))  // validaopcion devuelve 1 o 0
+            printf("Error ingrese opción de rango 1 al 4 \n"); // Nota:cambiar el printf por una funcion con defines de errores
+    }
+    while ( validaopcion(opcion));
+    switch(opcion) // adentro se va a trabajar las funciones principales
+    {
+        case 1:{
+             printf("Juego de dos jugadores");
+            break;
+        }
+        case 2:
+            break;
+        case 3: break;
+
+        case 4: return 0; // por ahora devolvemos 0 modificar luego
+
+
+    }
+
+    int validaopcion(int a) //ejemplo
+    {
+        if (a<1 || a>4)
+            return 1;
+        return 0;
+    }
 
 }
-int menu(void)
-{
-	int a;
-	do {
-    a=getint("Ingrese opcion\n");
-    if (validamenu(a))  //opcion<1 || opcion>4 devuelve 1 o 0
-    	printf("Error 1\n");
 
-	}
-	while (!validamenu(a))
-	return a;
-}
-int main ()
-{
-  int opcion;
-  opcion=menu();
-	switch (opcion)
-	{
-		case 1:
-		break;
-		case 2:
-		break;
-		case 3:
-		break;{
-		}
-		case 4:
-	
-			
-	}
-}
